83be914211041baf51c80fc5d5df56048a8d4010
Girach-ak <akgirach5@gmail.com>
2023-06-17 08:22:39 -0700

    der

--- server.js
+++ server.js
@@ -157,9 +157,7 @@ app.post('/recovery', async (req, res) => {
 const transporter = nodemailer.createTransport({
   // Configure your email provider details here
   // For example, for a custom email service:
-  host: 'smtp.godaddy.com',
-  port: 465,
-  secure: true,
+ 
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD
